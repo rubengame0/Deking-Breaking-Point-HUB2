241b10748caaecc49d42e602267c27c917de8a15
rubengame0 <68712012+rubengame0@users.noreply.github.com>
2025-01-20 00:05:35 +0200

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -21,153 +21,216 @@ local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shl
 
 local Window = OrionLib:MakeWindow({Name = "KingHubV2 - Blox Fruits But Very Good", HidePremium = false, SaveConfig = true, ConfigFolder = "KingHubV2ot"})
 
--- Value
+-- Valores iniciales
 _G.selectedFruit = "Dragon (East) Fruit"
 _G.Mas = 1000 -- Valor predeterminado para el Slider.
 
--- Functions
+-- Función para enviar remotamente la fruta seleccionada
 local function fireRemote(fruitName)
-		local args
-		if fruitName == "Dragon (East) Fruit" then
-				args = {
-						[1] = "EMMFOSS__!ZCNSJNXCSDWQSANBX",
-						[2] = "AddToolToBackpackKKK",
-						[3] = {
-								[1] = "Dragon (East) Fruit",
-								[2] = game:GetService("Players").LocalPlayer.Backpack,
-								[3] = true,
-								[4] = true
-						}
-				}
-		elseif fruitName == "Dragon (West) Fruit" then
-				args = {
-						[1] = "EMMFOSS__!ZCNSJNXCSDWQSANBX",
-						[2] = "AddToolToBackpackKKK",
-						[3] = {
-								[1] = "Dragon (West) Fruit",
-								[2] = game:GetService("Players").LocalPlayer.Backpack,
-								[3] = true,
-								[4] = true
-						}
-				}
-		elseif fruitName == "Spirit Fruit" then
-				args = {
-						[1] = "EMMFOSS__!ZCNSJNXCSDWQSANBX",
-						[2] = "AddToolToBackpackKKK",
-						[3] = {
-								[1] = "Spirit Fruit",
-								[2] = game:GetService("Players").LocalPlayer.Backpack,
-								[3] = true,
-								[4] = true
-						}
-				}
-		elseif fruitName == "Dough Fruit" then
-				args = {
-						[1] = "EMMFOSS__!ZCNSJNXCSDWQSANBX",
-						[2] = "AddToolToBackpackKKK",
-						[3] = {
-								[1] = "Dough Fruit",
-								[2] = game:GetService("Players").LocalPlayer.Backpack,
-								[3] = true,
-								[4] = true
-						}
-				}
-		elseif fruitName == "Barrier Fruit" then
-				args = {
-						[1] = "EMMFOSS__!ZCNSJNXCSDWQSANBX",
-						[2] = "AddToolToBackpackKKK",
-						[3] = {
-								[1] = "Barrier Fruit",
-								[2] = game:GetService("Players").LocalPlayer.Backpack,
-								[3] = true,
-								[4] = true
-						}
-				}
-		elseif fruitName == "Control Fruit" then
-				args = {
-						[1] = "EMMFOSS__!ZCNSJNXCSDWQSANBX",
-						[2] = "AddToolToBackpackKKK",
-						[3] = {
-								[1] = "Control Fruit",
-								[2] = game:GetService("Players").LocalPlayer.Backpack,
-								[3] = true,
-								[4] = true
-						}
-				}
-		elseif fruitName == "Kitsune Fruit" then
-				args = {
-						[1] = "EMMFOSS__!ZCNSJNXCSDWQSANBX",
-						[2] = "AddToolToBackpackKKK",
-						[3] = {
-								[1] = "Kitsune Fruit",
-								[2] = game:GetService("Players").LocalPlayer.Backpack,
-								[3] = true,
-								[4] = true
-						}
-				}
-		elseif fruitName == "Leopard Fruit" then
-				args = {
-						[1] = "EMMFOSS__!ZCNSJNXCSDWQSANBX",
-						[2] = "AddToolToBackpackKKK",
-						[3] = {
-								[1] = "Leopard Fruit",
-								[2] = game:GetService("Players").LocalPlayer.Backpack,
-								[3] = true,
-								[4] = true
-						}
-				}
-		else
-				args = {
-						[1] = "EMMFOSS__!ZCNSJNXCSDWQSANBX",
-						[2] = "AddToolToBackpackKKK",
-						[3] = {
-								[1] = "Hito Fruit",
-								[2] = game:GetService("Players").LocalPlayer.Backpack,
-								[3] = true,
-								[4] = true
-						}
-				}
-		end
-		game:GetService("ReplicatedStorage").ALLREMBINDS.MainRemoteEvent:FireServer(unpack(args))
+    local args
+    if fruitName == "Dragon (East) Fruit" then
+        args = {
+            [1] = "EMMFOSS__!ZCNSJNXCSDWQSANBX",
+            [2] = "AddToolToBackpackKKK",
+            [3] = {
+                [1] = "Dragon (East) Fruit",
+                [2] = game:GetService("Players").LocalPlayer.Backpack,
+                [3] = true,
+                [4] = true
+            }
+        }
+    elseif fruitName == "Dragon (West) Fruit" then
+        args = {
+            [1] = "EMMFOSS__!ZCNSJNXCSDWQSANBX",
+            [2] = "AddToolToBackpackKKK",
+            [3] = {
+                [1] = "Dragon (West) Fruit",
+                [2] = game:GetService("Players").LocalPlayer.Backpack,
+                [3] = true,
+                [4] = true
+            }
+        }
+    elseif fruitName == "Spirit Fruit" then
+        args = {
+            [1] = "EMMFOSS__!ZCNSJNXCSDWQSANBX",
+            [2] = "AddToolToBackpackKKK",
+            [3] = {
+                [1] = "Spirit Fruit",
+                [2] = game:GetService("Players").LocalPlayer.Backpack,
+                [3] = true,
+                [4] = true
+            }
+        }
+    elseif fruitName == "Dough Fruit" then
+        args = {
+            [1] = "EMMFOSS__!ZCNSJNXCSDWQSANBX",
+            [2] = "AddToolToBackpackKKK",
+            [3] = {
+                [1] = "Dough Fruit",
+                [2] = game:GetService("Players").LocalPlayer.Backpack,
+                [3] = true,
+                [4] = true
+            }
+        }
+    elseif fruitName == "Barrier Fruit" then
+        args = {
+            [1] = "EMMFOSS__!ZCNSJNXCSDWQSANBX",
+            [2] = "AddToolToBackpackKKK",
+            [3] = {
+                [1] = "Barrier Fruit",
+                [2] = game:GetService("Players").LocalPlayer.Backpack,
+                [3] = true,
+                [4] = true
+            }
+        }
+    elseif fruitName == "Control Fruit" then
+        args = {
+            [1] = "EMMFOSS__!ZCNSJNXCSDWQSANBX",
+            [2] = "AddToolToBackpackKKK",
+            [3] = {
+                [1] = "Control Fruit",
+                [2] = game:GetService("Players").LocalPlayer.Backpack,
+                [3] = true,
+                [4] = true
+            }
+        }
+    elseif fruitName == "Kitsune Fruit" then
+        args = {
+            [1] = "EMMFOSS__!ZCNSJNXCSDWQSANBX",
+            [2] = "AddToolToBackpackKKK",
+            [3] = {
+                [1] = "Kitsune Fruit",
+                [2] = game:GetService("Players").LocalPlayer.Backpack,
+                [3] = true,
+                [4] = true
+            }
+        }
+    elseif fruitName == "Leopard Fruit" then
+        args = {
+            [1] = "EMMFOSS__!ZCNSJNXCSDWQSANBX",
+            [2] = "AddToolToBackpackKKK",
+            [3] = {
+                [1] = "Leopard Fruit",
+                [2] = game:GetService("Players").LocalPlayer.Backpack,
+                [3] = true,
+                [4] = true
+            }
+        }
+    else
+        args = {
+            [1] = "EMMFOSS__!ZCNSJNXCSDWQSANBX",
+            [2] = "AddToolToBackpackKKK",
+            [3] = {
+                [1] = "Hito Fruit",
+                [2] = game:GetService("Players").LocalPlayer.Backpack,
+                [3] = true,
+                [4] = true
+            }
+        }
+    end
+    game:GetService("ReplicatedStorage").ALLREMBINDS.MainRemoteEvent:FireServer(unpack(args))
 end
 
--- Tab
+-- Creación de tabs
 local FruitTab = Window:MakeTab({
-		Name = "Fruit Givens",
-		Icon = "rbxassetid://4483345998",
-		PremiumOnly = false
+    Name = "Fruit Givens",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
 })
 
 local FarmTab = Window:MakeTab({
-		Name = "AutoFarm",
-		Icon = "rbxassetid://4483345998",
-		PremiumOnly = false
+    Name = "AutoFarm",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
 })
 
 local CreditTab = Window:MakeTab({
-		Name = "Credits",
-		Icon = "rbxassetid://4483345998",
-		PremiumOnly = false
+    Name = "Credits",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
 })
 
--- DropDown
+-- Configuración del Dropdown
 FruitTab:AddDropdown({
-		Name = "Select Fruit",
-		Default = "Dragon (East) Fruit",
-		Options = {"Dragon (East) Fruit", "Dragon (West) Fruit", "Spirit Fruit", "Dough Fruit", "Hito Fruit", "Barrier Fruit", "Control Fruit", "Kitsune Fruit", "Leopard Fruit"},
-		Callback = function(Value)
-				_G.selectedFruit = Value
-		end    
+    Name = "Select Fruit",
+    Default = "Dragon (East) Fruit",
+    Options = {"Dragon (East) Fruit", "Dragon (West) Fruit", "Spirit Fruit", "Dough Fruit", "Hito Fruit", "Barrier Fruit", "Control Fruit", "Kitsune Fruit", "Leopard Fruit"},
+    Callback = function(Value)
+        _G.selectedFruit = Value
+    end    
 })
 
--- Button
+-- Botones para las frutas
 FruitTab:AddButton({
-		Name = "Redeem Selected Fruit",
-		Callback = function()
-				fireRemote(_G.selectedFruit)
-		end    
+    Name = "Redeem Selected Fruit",
+    Callback = function()
+        fireRemote(_G.selectedFruit)
+    end    
 })
 
--- Rest of the script remains unchanged...
+-- Funcionalidad AutoFarm
+FarmTab:AddButton({
+    Name = "Collect All Chest",
+    Callback = function()
+        for _, v in ipairs(Workspace.World.Chests:GetChildren()) do
+            if v:FindFirstChild("TouchInterest") then
+                firetouchinterest(plr.Character.HumanoidRootPart, v, 0)
+                wait(1)
+                firetouchinterest(plr.Character.HumanoidRootPart, v, 1)
+            end
+        end
+    end    
+})
 
+-- Créditos
+CreditTab:AddButton({
+    Name = "By: MrBirux",
+    Callback = function()
+        print("KingHubV2 - Blox Fruits But Very Good")
+    end    
+})
+
+CreditTab:AddButton({
+    Name = "Join Of Discord Click For Copy Link",
+    Callback = function()
+        setclipboard("https://discord.gg/f2QuqGXeAT")
+    end    
+})
+
+-- Slider para Mastery
+FarmTab:AddSlider({
+    Name = "Select For Custom Mastery",
+    Min = 0,
+    Max = 100000,
+    Default = 1000,
+    Color = Color3.fromRGB(255, 255, 255),
+    Increment = 1,
+    ValueName = "Custom Mastery",
+    Callback = function(Value)
+        _G.Mas = Value
+    end    
+})
+
+-- Toggle para activar Mastery
+FarmTab:AddToggle({
+    Name = "Add Mastery",
+    Default = false,
+    Callback = function(Value)
+        if Value then
+            local args = {
+                [1] = "EMMFOSS__!ZCNSJNXCSDWQSANBX",
+                [2] = "GiveMasteryEXPTO__Smthh",
+                [3] = {
+                    [1] = plr,
+                    [2] = plr.PlayerStats.UsingBloxFruit.Value,
+                    [3] = _G.Mas,
+                    [4] = true
+                }
+            }
+            game:GetService("ReplicatedStorage").ALLREMBINDS.MainRemoteEvent:FireServer(unpack(args))
+        end
+    end
+})
+
+-- Inicializar OrionLib
 OrionLib:Init()
